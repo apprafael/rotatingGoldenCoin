543026d2949c4cbc0c03ef69cb6f673d3e74b632
Rafael Almeida <apprafael.almeida@gmail.com>
2023-01-07 13:45:56 -0300

    Initial Commit

---
+++ rotatingGoldenCoin/rotatingGoldenCoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  rotatingGoldenCoinApp.swift
+//  rotatingGoldenCoin
+//
+//  Created by Rafael Almeida on 1/7/23.
+//
+
+import SwiftUI
+
+@main
+struct rotatingGoldenCoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
